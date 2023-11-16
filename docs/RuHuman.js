726c244fdd768debe37444bbc787245988fcb61a
Timothy Do <timothydobsa@gmail.com>
2023-11-16 10:36:35 -0800

    Pushed some initial results online. Updated Propsal.html javascript call (content remains unchanged as shown in MD file).

---
+++ docs/RuHuman.js
@@ -0,0 +1,12 @@
+/* RuHuman Javascript Engine */
+/* Author: Timothy Do */
+
+/* Go to the Github Page */
+function goGithub() {
+	window.location.href = "https://github.com/dotimothy/RuHuman";
+}
+
+/* Go to the Home Page */
+function goHome() {
+	window.location.href = './index.html';
+}
