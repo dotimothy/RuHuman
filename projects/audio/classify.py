711b23900a2fec74e3dc2c09ddfb5f20a97cc719
Timothy Do <timothydobsa@gmail.com>
2023-12-12 12:08:48 -0800

    Uploaded the Pretrained Tortoise classifier model along with a test script classify.py (will be UI default)

---
+++ projects/audio/classify.py
@@ -0,0 +1,36 @@
+import argparse
+
+import torch
+import torch.nn.functional as F
+from tortoise.utils.audio import load_audio
+from tortoise.models.classifier import AudioMiniEncoderWithClassifierHead
+import os
+
+
+def classify_audio_clip(clip):
+    
+    """
+    Returns whether or not Tortoises' classifier thinks the given clip came from Tortoise.
+    :param clip: torch tensor containing audio waveform data (get it from load_audio)
+    :return: True if the clip was classified as coming from Tortoise and false if it was classified as real.
+    """
+    modelPath = './Detectors/Tortoise/classifier.pth'
+    classifier = AudioMiniEncoderWithClassifierHead(2, spec_dim=1, embedding_dim=512, depth=5, downsample_factor=4,
+                                                    resnet_blocks=2, attn_blocks=4, num_attn_heads=4, base_channels=32,
+                                                    dropout=0, kernel_size=5, distribute_zero_label=False)
+    classifier.load_state_dict(torch.load(modelPath, map_location=torch.device('cuda')))
+    clip = clip.cpu().unsqueeze(0)
+    results = F.softmax(classifier(clip), dim=-1)
+    return results[0][0]
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clip', type=str, help='Path to an audio clip to classify.', default="./JR.wav")
+    args = parser.parse_args()
+
+    clip = load_audio(args.clip, 24000)
+    clip = clip[:, :220000]
+    prob = classify_audio_clip(clip)
+    print(f"This classifier thinks there is a {prob*100}% chance that this clip was generated from Tortoise.")
